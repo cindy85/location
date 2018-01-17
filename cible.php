6aab7f4d5b6d7d6ee8ac79bab516450a01b66f89
cindy85 <33489668+cindy85@users.noreply.github.com>
2018-01-17 17:58:25 +0100

    parametrage php formulaire

---
+++ cible.php
@@ -0,0 +1,21 @@
+<?php
+header('content-type: text/html; charset=utf-8');
+if(isset($_POST["btn"])){
+    $name= $_POST['nom'];
+$email= $_POST['email'];
+$telephone= $_POST['telephone'];
+$message= $_POST['message'];
+$destinataire="cindyb85@hotmail.fr;sylvie.bogaert015@orange.fr";
+$sujet="contact site web";
+$entete='From:'.$email.'';
+$message ='nom:'.$nom.''."\n".
+            'telephone: '.$telephone.''."\n".
+    'objet:'.$objet.''."\n".
+    'contenumessage : '.$contenumessage.'';
+mail($destinataire, $sujet, $message, $entete);
+header('location:https://www.location-maison-vacances-a-la-mer/message-envoi.html');
+}
+else{
+echo 'erreur de message'; 
+}
+?>
